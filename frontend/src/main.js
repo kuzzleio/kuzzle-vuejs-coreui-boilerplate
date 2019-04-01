29e964dd09d55d6901cc69b75b2209c18d58866b
njuelle <n.juelle@gmail.com>
2019-04-01 14:08:15 +0200

    [npm] add vue-kuzzle plugin

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,18 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
+import { createRouter } from './router';
 import store from './store';
 import i18n from './i18n';
 import './notifications';
 import BootstrapVue from 'bootstrap-vue';
+import VueKuzzle from 'vue-kuzzle';
+import config from './config.json';
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
+Vue.use(VueKuzzle, config);
+
+const router = createRouter(Vue.prototype.$kuzzle, store);
 
 new Vue({
   router,
