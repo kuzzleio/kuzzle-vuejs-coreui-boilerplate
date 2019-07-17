0736a5a05c683e418b176e044ddf2ff91c196e3c
Nicolas Juelle <n.juelle@gmail.com>
2019-07-17 17:15:04 +0200

    [Front] Better CoreUI integration (#4)
    What does this PR do ?
    Add a better CoreUI integration, with a nice homepage full of useful kuzzle links
    
    because this boilerplate was supposed to use CoreUI but it was just installed in dependencies and never used in the example page
    
    How should this be manually tested?
    $ npm run serve
    
    Other changes
    I also cleaned auth state and remove user_locale because it was a residue of a client project in which we save the locale field directly in the user content
    
    Boyscout
    Better html for LoginFom component

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Kuzzle + Vue.js App"/>
+    <img alt="Vue logo" src="../assets/kuzzle_vue.png" />
+    <HelloWorld msg="Welcome to Your Kuzzle + Vue.js App" />
   </div>
 </template>
 
