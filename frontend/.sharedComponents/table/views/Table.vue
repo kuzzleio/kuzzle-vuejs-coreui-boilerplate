db47ed66a74976894c083bb9b8d9e332d0b8d704
jenow <jenova.ke20@gmail.com>
2021-01-13 15:33:07 +0100

    add crud for table

--- frontend/.sharedComponents/table/views/Table.vue
+++ frontend/.sharedComponents/table/views/Table.vue
@@ -16,31 +16,59 @@
           @page-changed="onPageChanged"
           @per-page-changed="onPerPageChanged"
         >
-          <template v-slot:cell(fullName)="data">
-            {{ data.item.firstname }} {{ data.item.lastname }}
-          </template>
-
           <template v-slot:cell(actions)="data">
-            <a href="#" @click.prevent="removeRow(data)" class="text-danger">
-              <i class="pointer fa fa-trash trash" />
+            <a
+              href="#"
+              @click.prevent="editRow(data)"
+              class="text-info"
+              v-b-tooltip.hover
+              title="Edit"
+            >
+              <i class="pointer fa fa-pen" /> </a
+            >&nbsp;
+            <a
+              href="#"
+              @click.prevent="removeRow(data)"
+              class="text-danger"
+              v-b-tooltip.hover
+              title="Delete"
+            >
+              <i class="pointer fa fa-trash" />
             </a>
           </template>
         </Table>
       </b-col>
     </b-row>
+    <b-row>
+      <b-col cols="2" align-self="start">
+        <b-button variant="info" @click="createRow">Add an item</b-button>
+      </b-col>
+    </b-row>
+
+    <b-modal id="modal" :title="modalAction" @hide="modalHidden">
+      <b-row>
+        <b-col cols="6">
+          <vue-form-generator :schema="formSchema" :model="document.metadata">
+          </vue-form-generator>
+        </b-col>
+      </b-row>
+    </b-modal>
   </b-container>
 </template>
 
 <script>
-import { ref, reactive, watch } from '@vue/composition-api';
-import Table from '@/components/Table.vue';
+import { ref, reactive } from '@vue/composition-api';
+import VueFormGenerator from 'vue-form-generator';
 
+import Table from '@/components/Table.vue';
 import MappingFieldsService from '@/services/MappingFieldsService';
+import { formSchemaService } from '@/services/formSchema';
 
 export default {
   name: 'TableView',
   components: {
-    Table
+    Table,
+    VueFormGenerator: VueFormGenerator.component
   },
   setup(props, ctx) {
     const mappingFieldsService = new MappingFieldsService();
@@ -50,6 +78,10 @@ export default {
     let perPage = ref(1);
     let filter = ref('');
     let sort = ref(null);
+    let formSchema = ref({});
+    let fields = ref([]);
+    let document = ref({});
+    let modalAction = ref('');
 
     const fetchItems = () => {
       let query = {};
@@ -69,29 +101,42 @@ export default {
       }
 
       ctx.root.$kuzzle.document
-        .search('cypress', 'e2e', query, {
+        .search('tenant1', 'asset', query, {
           from: (currentPage - 1) * perPage.value,
           size: perPage.value
         })
         .then(res => {
           items.splice(items, items.length);
           for (const doc of res.hits) {
-            items.push(doc._source);
+            items.push({
+              _id: doc._id,
+              ...doc._source
+            });
           }
           totalRows.value = res.total;
+          document.value = JSON.parse(JSON.stringify(items[0]));
+
+          ctx.root.$kuzzle.collection
+            .getMapping('tenant1', 'asset', {
+              includeKuzzleMeta: false
+            })
+            .then(mapping => {
+              fields.value = mappingFieldsService.getFieldsForTable(mapping);
+              fields.value.push({
+                key: 'actions',
+                label: 'Actions',
+                sortable: false
+              });
+
+              formSchema.value = formSchemaService.generate(
+                mapping.properties.metadata.properties,
+                document.value.metadata
+              );
+            });
         });
     };
 
     fetchItems();
-
-    let fields = ref([]);
-    ctx.root.$kuzzle.collection
-      .getMapping('cypress', 'e2e', {
-        includeKuzzleMeta: false
-      })
-      .then(res => {
-        fields.value = mappingFieldsService.getFieldsForTable(res);
-      });
 
     return {
       items,
@@ -99,8 +144,64 @@ export default {
       perPage,
       totalRows,
       fields,
+      formSchema,
+      document,
+      modalAction,
+      editRow: data => {
+        document.value = JSON.parse(JSON.stringify(data.item));
+        modalAction.value = 'Edit';
+        ctx.root.$bvModal.show('modal');
+      },
+      createRow: data => {
+        document.value = {
+          metadata: {}
+        };
+        modalAction.value = 'Create';
+        ctx.root.$bvModal.show('modal');
+      },
+      modalHidden: async event => {
+        if (event.trigger === 'ok') {
+          if (document.value && document.value._id) {
+            const id = document.value._id;
+            delete document.value._id;
+            await ctx.root.$kuzzle.document.update(
+              'tenant1',
+              'asset',
+              id,
+              document.value,
+              {
+                refresh: 'wait_for'
+              }
+            );
+          } else {
+            await ctx.root.$kuzzle.document.create(
+              'tenant1',
+              'asset',
+              document.value,
+              '',
+              {
+                refresh: 'wait_for'
+              }
+            );
+          }
+
+          fetchItems();
+        }
+      },
       removeRow: data => {
-        console.log(data.item);
+        ctx.root.$bvModal.msgBoxConfirm('Are you sure?').then(async value => {
+          if (value) {
+            await ctx.root.$kuzzle.document.delete(
+              'tenant1',
+              'asset',
+              data.item._id,
+              {
+                refresh: 'wait_for'
+              }
+            );
+          }
+          fetchItems();
+        });
       },
       handleEvent: data => {
         console.log(data);
